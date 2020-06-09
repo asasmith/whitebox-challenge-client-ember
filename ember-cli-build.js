d7d6285531ebb25e32088fe7ff26788a1978f54a
asasmith <smith.asa.la@gmail.com>
2020-06-09 16:59:59 -0400

    add tailwindcss and basic styles

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,39 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { join } = require('path');
+const { WatchedDir } = require('broccoli-source');
+const compilePostCSS = require('broccoli-postcss-single');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
 
+  const tailwindBase = join(__dirname, 'app', 'tailwind');
+  const plugins = [require('tailwindcss')(join(tailwindBase, 'config.js'))];
+  console.log(plugins)
+
+  if (EmberApp.env() === 'production') {
+    plugins.push({
+        module: require('@fullhuman/postcss-purgecss'),
+        options: [
+          join(__dirname, 'app', 'index.html'),
+          join(__dirname, 'app', '**', '*.js'),
+          join(__dirname, 'app', '**', '*.hbs'),
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        whitelistPattern: [/^_/],
+    });
+  }
+
+  const tailwindNode = compilePostCSS(
+    new WatchedDir(tailwindBase),
+    'index.css',
+    'assets/tailwind.css',
+    { /*browsers,*/ cacheInclude: [/.*\.(css|js)$/], plugins},
+  );
+  
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
@@ -20,5 +47,5 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  return app.toTree([tailwindNode]);
 };
